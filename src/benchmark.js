ad54adc2738923c051271de96273989d3904f524
jrprice <jamesprice.dev@gmail.com>
2022-01-05 01:42:53 +0000

    Deploying to gh-pages from  @ d3d0c674728f45f06c1f34fecb37aafee78f8cda 🚀

--- src/benchmark.js
+++ src/benchmark.js
@@ -219,8 +219,9 @@ fn store(i : u32, value : u32) {
     const ms = end - start;
     const dataMoved = arraySize * bytesPerElement * 2 * iterations;
     const bytesPerSecond = dataMoved / (ms / 1000.0);
-    const gbytesPerSecond = (bytesPerSecond * 1e-9).toFixed(1);
-    setStatus(name, ms.toFixed(1) + ` ms  (${gbytesPerSecond} GB/s)`);
+    const msStr = ms.toFixed(1).padStart(7);
+    const gbsStr = (bytesPerSecond * 1e-9).toFixed(1).padStart(6);
+    setStatus(name, msStr + ` ms   ${gbsStr} GB/s`);
 });
 function validate(name, type, bytesPerElement) {
     return __awaiter(this, void 0, void 0, function* () {
